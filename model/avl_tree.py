2efb3454047784a36940ea1d5789ffe8dd808b09
Italo Alves <italo.alves.jp@gmail.com>
2020-12-09 03:52:56 -0300

    Atualiza arvore AVL

--- model/avl_tree.py
+++ model/avl_tree.py
@@ -1,3 +1,6 @@
+from model.node import Node
+
+
 class AVLTree():
     def __init__(self):
         self.node = None
@@ -7,6 +10,25 @@ class AVLTree():
     @property
     def height(self):
         if self.node:
-            return self.node.height
+            return self.height
+
+    def insert(self, data):
+        tree = self.node
+
+        new_node = Node(data)
+
+        if tree == None:
+            self.node = new_node
+            self.node.left = AVLTree()
+            self.node.right = AVLTree()
+
+        elif data.id < tree.data.id:
+            self.node.left.insert(data.id)
+
+        elif data.id > tree.data.id:
+            self.node.right.insert(data.id)
+
         else:
-            return 0
+            raise("ID [" + str(data.id) + "] already exists in tree.")
+
+        # self.rebalance()
